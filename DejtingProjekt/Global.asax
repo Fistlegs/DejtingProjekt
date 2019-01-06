cd947a9033fcfb13cd781e25fb048b699b556458
Fistlegs <Vince1@live.se>
2019-01-06 17:52:41 +0100

    faf

---
+++ DejtingProjekt/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="DejtingProjekt.MvcApplication" Language="C#" %>
---
+++ DejtingProjekt/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(DejtingProjekt.Startup))]
+namespace DejtingProjekt
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
