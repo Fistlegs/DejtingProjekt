2177dd6992262ff9f53e7b5c6f12e2893493a2e4
Fistlegs <Vince1@live.se>
2019-01-06 17:52:49 +0100

    dad

---
+++ DejtingProjekt/Controllers/ProfileController.cs
@@ -0,0 +1,33 @@
+﻿using DejtingProjekt.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace DejtingProjekt.Controllers
+{
+    public class ProfileController : Controller
+    {
+        // GET: Profile
+        public ActionResult Index()
+        {
+            var ctx = new DejtDBContext();
+            var viewModel = new ProfileIndexViewModel
+            {
+                Profiles = ctx.Profiles.ToList()
+            };
+            return View(viewModel);
+        }
+
+        [HttpPost]
+        public ActionResult AddProfile(ProfileModel model)
+        {
+            var ctx = new DejtDBContext();
+            ctx.Profiles.Add(model);
+            ctx.SaveChanges();
+
+            return RedirectToAction("Index");
+        }
+    }
+}
---
+++ DejtingProjekt/Models/DejtDBContext.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Linq;
+using System.Web;
+
+namespace DejtingProjekt.Models
+{
+    public class DejtDBContext : DbContext
+    {
+        public DbSet<ProfileModel> Profiles { get; set; }
+        public DbSet<PostModel> Posts { get; set; }
+
+        public DejtDBContext() : base("DejtDB") { }
+    }
+}
